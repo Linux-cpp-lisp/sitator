4dcf677ec61f6f3ec1087752b1775c8b2279972a
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2018-07-27 14:54:04 +0200

    Added VoronoiSiteGenerator

---
+++ voronoi/VoronoiSiteGenerator.py
@@ -0,0 +1,26 @@
+
+import numpy as np
+
+from ..util import Zeopy
+
+DEFAULT_ZEOPP = "/home/amusaelian/Documents/Ionic Frustration/code/lib/zeo++/trunk/network"
+
+def VoronoiSiteGenerator(object):
+    """Given an empty SiteNetwork, use the Voronoi decomposition to predict/generate sites."""
+
+    def __init__(self, radial = False, verbose = True, zeopp_path = DEFAULT_ZEOPP):
+        self._radial = radial
+        self._verbose = verbose
+        self._zeopy = Zeopy(zeopp_path)
+
+    def run(self, sn):
+        """SiteNetwork -> SiteNetwork"""
+
+        nodes, verts, edges, _ = self._zeopy.voronoi(sn._static_structure,
+                                                    radial = self._radial,
+                                                    verbose = self._verbose)
+
+        out = sn.copy()
+        out.set_sites(nodes, verts)
+
+        return out
