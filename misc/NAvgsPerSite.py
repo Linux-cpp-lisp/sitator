00f037d9e97f6e02e87520f7831887c0a5de7120
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2018-07-28 14:12:37 +0200

    Fixed NAvgsPerSite

--- misc/NAvgsPerSite.py
+++ misc/NAvgsPerSite.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 
+from analysis import SiteNetwork, SiteTrajectory
 from analysis.util import PBCCalculator
 
 class NAvgsPerSite(object):
@@ -8,15 +9,17 @@ class NAvgsPerSite(object):
 
     The `types` of sites in the output are the index of the site in the input that generated
     that average.
+
+    :param int n: How many averages to take
+    :param bool error_on_insufficient: Whether to throw an error if n points cannot
+        be provided for a site, or just take all that are available.
+    :param bool weighted: Use SiteTrajectory confidences to weight the averages.
     """
 
     def __init__(self, n,
-                 error_on_insufficient = False,
+                 error_on_insufficient = True,
                  weighted = True):
-        """
-        :params int n: How many avgs. to take
-        :
-        """
+        assert n % 2 == 0
         self.n = n
         self.error_on_insufficient = error_on_insufficient
         self.weighted = weighted
@@ -37,41 +40,26 @@ class NAvgsPerSite(object):
                 pts, confs = st.real_positions_for_site(site, return_confidences = True)
             else:
                 pts = st.real_positions_for_site(site)
+                confs = np.ones(shape = len(pts), dtype = np.int)
 
             old_idex = current_idex
 
-            if len(pts) < self.n:
+            if len(pts) > self.n:
+                sanity = 0
+                for i in xrange(self.n):
+                    ps = pts[i::self.n]
+                    sanity += len(ps)
+                    c = confs[i::self.n]
+                    centers[current_idex] = pbcc.average(ps, weights = c)
+                    current_idex += 1
+
+                assert sanity == len(pts)
+                assert current_idex - old_idex == self.n
+            else:
                 if self.error_on_insufficient:
                     raise ValueError("Insufficient points assigned to site %i (%i) to take %i averages." % (site, len(pts), self.n))
                 centers[current_idex:current_idex + len(pts)] = pts
                 current_idex += len(pts)
-            else:
-                # Actually do averages
-                avg_len = int(np.floor(len(pts) / self.n))
-                if avg_len % 2 != 0: avg_len -= 1
-                n_avg, n_take = np.divmod(len(pts), avg_len)
-                half = avg_len / 2
-
-                buf = np.empty(shape = (avg_len, 3), dtype = st.real_trajectory.dtype)
-                buf.fill(np.nan)
-
-                confbuf = np.ones(shape = avg_len, dtype = np.float)
-                for i in xrange(n_avg):
-                    buf[:half] = pts[i * avg_len:i * avg_len + half]
-                    half2end = -(i * avg_len)
-                    if half2end == 0: half2end = None
-                    buf[half:] = pts[-(i * avg_len + half):half2end]
-                    if self.weighted:
-                        confbuf[:half] = confs[i * avg_len:i * avg_len + half]
-                        confbuf[half:] = confs[-(i * avg_len + half):half2end]
-                    centers[current_idex] = pbcc.average(buf, weights = confbuf)
-                    current_idex += 1
-
-                for pt in pts[half * n_avg:-(half * avg_len)]:
-                    centers[current_idex] = pt
-                    current_idex += 1
-
-                assert current_idex - old_idex == self.n
 
             types[old_idex:current_idex] = site
 
