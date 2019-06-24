da31818a77ac6d4cd0c8be69b8eeac5f3d956b16
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2019-06-24 10:13:52 -0400

    Refactoring

--- sitator/SiteTrajectory.py
+++ sitator/SiteTrajectory.py
@@ -88,6 +88,7 @@ class SiteTrajectory(object):
     def real_trajectory(self):
         return self._real_traj
 
+
     def set_real_traj(self, real_traj):
         """Assocaite this SiteTrajectory with a trajectory of points in real space.
 
@@ -98,10 +99,12 @@ class SiteTrajectory(object):
             raise ValueError("real_traj of shape %s does not have expected shape %s" % (real_traj.shape, expected_shape))
         self._real_traj = real_traj
 
+
     def remove_real_traj(self):
         """Forget associated real trajectory."""
         del self._real_traj
         self._real_traj = None
+
 
     def trajectory_for_particle(self, i, return_confidences = False):
         """Returns the array of sites particle i is assigned to over time."""
@@ -111,6 +114,7 @@ class SiteTrajectory(object):
             return self._traj[:, i], self._confs[:, i]
         else:
             return self._traj[:, i]
+
 
     def real_positions_for_site(self, site, return_confidences = False):
         if self._real_traj is None:
@@ -129,90 +133,12 @@ class SiteTrajectory(object):
         else:
             return pts
 
+
     def compute_site_occupancies(self):
         """Computes site occupancies and adds site attribute `occupancies` to site_network."""
         occ = np.true_divide(np.bincount(self._traj[self._traj >= 0], minlength = self._sn.n_sites), self.n_frames)
         self.site_network.add_site_attribute('occupancies', occ)
         return occ
-
-    def clamped_real_trajectory(self, clamp_mask = None, wrap = False, pass_through_unassigned = False):
-        """Create a real-space trajectory with the fixed site/static structure positions.
-
-        Generate a real-space trajectory where the atoms indicated in `clamp_mask` --
-        any mixture of static and mobile -- are clamped to: (1) the fixed position of
-        their current site, if mobile, or (2) the corresponding fixed position in
-        the `SiteNetwork`'s static structure, if static.
-
-        Atoms not indicated in `clamp_mask` will have their positions from `real_traj`
-        passed through.
-
-        Clamped positions will be in the unit cell; it is assumed that the real
-        trajectory is wrapped.
-
-        Args:
-            - clamp_mask (ndarray, len(sn.structure))
-            - wrap (bool, default: False)
-            - pass_through_unassigned (bool, default: False): If True, when a
-                mobile atom is supposed to be clamped but is unassigned at some
-                frame, its real-space position will be passed through from the
-                real trajectory. If False, an error will be raised.
-        Returns:
-            ndarray (n_frames x n_atoms x 3)
-        """
-        cell = self._sn.structure.cell
-        pbcc = PBCCalculator(cell)
-
-        n_atoms = len(self._sn.structure)
-        if clamp_mask is None:
-            clamp_mask = np.ones(shape = n_atoms, dtype = np.bool)
-        if self._real_traj is None and not np.all(clamp_mask):
-            raise RuntimeError("This `SiteTrajectory` has no real-space trajectory, but the given clamp mask leaves some atoms unclamped.")
-
-        clamptrj = np.empty(shape = (self.n_frames, n_atoms, 3))
-        # Pass through unclamped positions
-        if not np.all(clamp_mask):
-            clamptrj[:, ~clamp_mask, :] = self._real_traj[:, ~clamp_mask, :]
-        # Clamp static atoms
-        static_clamp = clamp_mask & self._sn.static_mask
-        clamptrj[:, static_clamp, :] = self._sn.structure.get_positions()[static_clamp]
-        # Clamp mobile atoms
-        mobile_clamp = clamp_mask & self._sn.mobile_mask
-        selected_sitetraj = self._traj[:, mobile_clamp]
-        mobile_clamp_indexes = np.where(mobile_clamp)[0]
-        if not pass_through_unassigned and np.min(selected_sitetraj) < 0:
-            raise RuntimeError("The mobile atoms indicated for clamping are unassigned at some point during the trajectory and `pass_through_unassigned` is set to False. Try `assign_to_last_known_site()`?")
-
-        if wrap:
-            for frame_i in tqdm(range(len(clamptrj))):
-                for mobile_i in mobile_clamp_indexes:
-                    at_site = self._traj[frame_i, mobile_i]
-                    if at_site == SiteTrajectory.SITE_UNKNOWN: # we already know that this means pass_through_unassigned = True
-                        clamptrj[frame_i, mobile_i] = self._real_traj[frame_i, mobile_i]
-                        continue
-                    clamptrj[frame_i, mobile_i] = self._sn.centers[at_site]
-        else:
-            buf = np.empty(shape = (1, 3))
-            site_pt = np.empty(shape = 3)
-            for frame_i in tqdm(range(len(clamptrj))):
-                for mobile_i in mobile_clamp_indexes:
-                    buf[:, :] = self._real_traj[frame_i, mobile_i]
-                    at_site = self._traj[frame_i, mobile_i]
-                    if at_site == SiteTrajectory.SITE_UNKNOWN: # we already know that this means pass_through_unassigned = True
-                        clamptrj[frame_i, mobile_i] = self._real_traj[frame_i, mobile_i]
-                        continue
-                    site_pt[:] = self._sn.centers[at_site]
-                    pbcc.wrap_point(site_pt)
-                    pbcc.wrap_points(buf)
-                    site_mic = pbcc.min_image(buf[0], site_pt)
-                    site_mic = [(site_mic // 10**(2 - i) % 10) - 1 for i in range(3)]
-                    buf[:, :] = self._real_traj[frame_i, mobile_i]
-                    pbcc.to_cell_coords(buf)
-                    pt_in_image = np.floor(buf[0])
-                    pt_in_image += site_mic
-                    # np.dot(np.matrix(cell.T), pt_in_image) +
-                    clamptrj[frame_i, mobile_i] =  np.dot(pt_in_image, cell) + self._sn.centers[at_site]
-
-        return clamptrj
 
 
     def assign_to_last_known_site(self, frame_threshold = 1):
