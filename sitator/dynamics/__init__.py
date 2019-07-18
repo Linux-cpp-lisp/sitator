f95c697164a648f248623c53cb757edc2d90834e
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2019-07-17 23:48:47 -0400

    Import correction

--- sitator/dynamics/__init__.py
+++ sitator/dynamics/__init__.py
@@ -2,7 +2,7 @@ from .JumpAnalysis import JumpAnalysis
 from .MergeSitesByDynamics import MergeSitesByDynamics
 from .MergeSitesByThreshold import MergeSitesByThreshold
 from .RemoveUnoccupiedSites import RemoveUnoccupiedSites
-from .RemoveShortJumps import RemoveShortJumps
+from .SmoothSiteTrajectory import SmoothSiteTrajectory
 from .AverageVibrationalFrequency import AverageVibrationalFrequency
 
 # For backwards compatability, since this used to be in this module
