dc01bdccb25d71dbdaa240ece55a074cfcc3bd51
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2019-07-25 17:19:27 -0400

    Added feature to keep short transitional jumps while removing failed attempts

--- sitator/dynamics/SmoothSiteTrajectory.pyx
+++ sitator/dynamics/SmoothSiteTrajectory.pyx
@@ -15,20 +15,33 @@ class SmoothSiteTrajectory(object):
 
     For each mobile particle, the assignmet at each frame is replaced by the
     mode of its site assignments over some number of frames centered around it.
-    If the multiplicity of the mode is less than the threshold, the particle is
-    marked unassigned at that frame.
 
     Can be thought of as a discrete lowpass filter.
 
+    The ``set_unassigned_under_threshold`` parameter allows the user to control
+    how the smoothing handles "transitions" vs. "attempts"; setting it to True,
+    the default, will mark as unassigned transitional moments where neither
+    the source nor destination site have a sufficient (``threshold``) majority
+    in the window, while setting it to False will maintain the assignment to a
+    transitional site.
+
     Args:
+        window_threshold_factor (float): The total width of the rolling window,
+            in terms of the threshold.
         remove_unoccupied_sites (bool): If True, sites that are unoccupied after
-            removing short jumps will be removed.
+            the smoothing will be removed.
+        set_unassigned_under_threshold (bool): If True, if the multiplicity of
+            the mode is less than the threshold, the particle is marked
+            unassigned at that frame. If False, the particle's assignment will
+            not be modified.
     """
     def __init__(self,
                  window_threshold_factor = 2.1,
-                 remove_unoccupied_sites = True):
+                 remove_unoccupied_sites = True,
+                 set_unassigned_under_threshold = True):
         self.window_threshold_factor = window_threshold_factor
         self.remove_unoccupied_sites = remove_unoccupied_sites
+        self.set_unassigned_under_threshold = set_unassigned_under_threshold
 
     def run(self,
             st,
@@ -43,7 +56,15 @@ class SmoothSiteTrajectory(object):
         window = self.window_threshold_factor * threshold
         wleft, wright = int(np.floor(window / 2)), int(np.ceil(window / 2))
 
-        running_windowed_mode(traj, out, wleft, wright, threshold, n_sites)
+        running_windowed_mode(
+            traj,
+            out,
+            wleft,
+            wright,
+            threshold,
+            n_sites,
+            self.set_unassigned_under_threshold
+        )
 
         st = st.copy(with_computed = False)
         st._traj = out
@@ -60,7 +81,8 @@ cpdef running_windowed_mode(site_int [:, :] traj,
                             Py_ssize_t wleft,
                             Py_ssize_t wright,
                             Py_ssize_t threshold,
-                            Py_ssize_t n_sites):
+                            Py_ssize_t n_sites,
+                            bint replace_no_winner_unknown):
     countbuf_np = np.zeros(shape = n_sites + 1, dtype = np.int)
     cdef Py_ssize_t [:] countbuf = countbuf_np
     cdef Py_ssize_t n_mobile = traj.shape[1]
@@ -82,5 +104,8 @@ cpdef running_windowed_mode(site_int [:, :] traj,
             if best_count >= threshold:
                 out[frame, mob] = winner - 1
             else:
-                out[frame, mob] = s_unknown
+                if replace_no_winner_unknown:
+                    out[frame, mob] = s_unknown
+                else:
+                    out[frame, mob] = traj[frame, mob]
             countbuf[:] = 0
