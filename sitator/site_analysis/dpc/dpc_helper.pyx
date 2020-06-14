97a66202edf5dd59b2953b2c503f653a524aa188
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2020-06-14 17:48:26 -0600

    Gridded DPC working

--- sitator/site_analysis/dpc/dpc_helper.pyx
+++ sitator/site_analysis/dpc/dpc_helper.pyx
@@ -56,9 +56,10 @@ cpdef dpc_compute_vars(frames_np,
     cdef Py_ssize_t n_boxes[3]
     n_boxes[:] = density.shape
     cdef Py_ssize_t max_n_boxes = np.max(density.shape)
-    cdef Py_ssize_t box_order[3]
-    box_order[:] = np.argsort(box_dims) # Smallest first
-    cdef cell_precision [:, :] box_vecs = pbcc.cell / n_boxes
+    # cdef Py_ssize_t box_order[3]
+    # box_order[:] = np.argsort(box_dims) # Smallest first
+    box_vecs_np =  pbcc.cell / n_boxes
+    cdef cell_precision [:, :] box_vecs = box_vecs_np
 
     deltas_np = np.full(density_np.shape, np.inf, dtype = frames_np.dtype)
     cdef precision [:, :, :] deltas = deltas_np
@@ -73,21 +74,19 @@ cpdef dpc_compute_vars(frames_np,
     # Their gammas
     gamma_buf_np = np.full(n_gamma, -np.inf, dtype = frames_np.dtype)
     cdef precision [:] gamma_buf = gamma_buf_np
-    cdef Py_ssize_t offset[3]
-    cdef bint done = False
-    cdef Py_ssize_t radius, component, temp1, temp2, side_i, side_j
-    cdef int side_sign
-    cdef Py_ssize_t offset_idex[3]
-    cdef precision offset_vec[3]
-    cdef precision gamma, curr_dist
-    cdef Py_ssize_t lookup_diff_component[3]
-    lookup_diff_component[0] = 1; lookup_diff_component[1] = 2; lookup_diff_component[2] = 0
+    #cdef Py_ssize_t offset[3]
+    #cdef Py_ssize_t radius, component, temp1, temp2, side_i, side_j
+    #cdef int side_sign
+    #cdef Py_ssize_t offset_idex[3]
+    #cdef precision offset_vec[3]
+    cdef precision gamma #, curr_dist
+
+    cdef precision curr_delta
+    cdef Py_ssize_t curr_delta_target
 
     for i in range(n_boxes[0]):
         for j in range(n_boxes[1]):
             for k in range(n_boxes[2]):
-                offset[0] = 0; offset[1] = 0; offset[2] = 0
-                done = False
                 if density[i, j, k] >= max_density:
                     # In the non periodic case, Rodriguez and Laio
                     # customarily take this to be the maximum pairwise
@@ -99,12 +98,10 @@ cpdef dpc_compute_vars(frames_np,
                     # We compute it here since this branch is only ever taken once.
                     deltas[i, j, k] = np.min(pbcc.cell_vector_lengths)
                     delta_targets[i, j, k] = -1
-                    done = True
                 elif density[i, j, k] == 0:
                     # We don't care about 0's, they're sparse:
                     deltas[i, j, k] = NAN
                     delta_targets[i, j, k] = -1
-                    done = True
                     # Skip gamma computations
                     continue
                 else:
@@ -116,52 +113,19 @@ cpdef dpc_compute_vars(frames_np,
                     #   # * #
                     #   # # #
                     # etc.
-                    for radius in range(1, max_n_boxes//2 + 1): # For each box size
-                        for component in range(3): # For each box dimension
-                            for side_sign in range(-1, 2, 2): # For each box side --- -1 and 1
-                                for side_i in range(-radius, radius + 1):
-                                    for side_j in range(-radius, radius + 1):
-                                        # Build an offset
-                                        offset[0] = side_i; offset[1] = side_i; offset[2] = side_i
-                                        offset[component] = side_sign * radius
-                                        offset[lookup_diff_component[component]] = side_j
-                                        # % deals with periodicity
-                                        offset_idex[0] = (i + offset[0]) % n_boxes[0]
-                                        offset_idex[1] = (j + offset[1]) % n_boxes[1]
-                                        offset_idex[2] = (k + offset[2]) % n_boxes[2]
-                                        # Compute the offset vector
-                                        for temp1 in range(3):
-                                            offset_vec[temp1] = 0
-                                            for temp2 in range(3):
-                                                offset_vec[temp1] += offset[temp2] * \
-                                                                     box_vecs[temp2, temp1]
-                                        curr_dist = sqrt(
-                                            offset_vec[0]*offset_vec[0] + \
-                                            offset_vec[1]*offset_vec[1] + \
-                                            offset_vec[2]*offset_vec[2]
-                                        )
-
-                                        if density[offset_idex[0], offset_idex[1], offset_idex[2]] > density[i, j, k] and \
-                                           curr_dist < deltas[i, j, k]: # Must be closer to be the new delta
-                                            # we found a better delta
-                                            deltas[i, j, k] = curr_dist # Set the new delta
-                                            delta_targets[i, j, k] = to_linear_index_3D(offset_idex, n_boxes)
-                                            # We've found the "up gradient" for this box.
-                                            # Break out and move to the next:
-                                            done = True
-                                        # == end for side_j
-                                    # == end for side_i
-                                # == end for side_sign
-                            # == end for component
-                        # We check the whole cube first, but if we've found a
-                        # delta at this radius, is is the delta: no larger
-                        # radius is going to be closer.
-                        if done:
-                            break
-                        # == end for radius
+                    curr_delta, curr_delta_target = min_dist_for_relation_pbc(
+                        i,j,k,
+                        density,
+                        n_boxes, box_vecs,
+                        RELATION_GT,
+                        max_n_boxes//2 + 1, # max radius,
+                    )
+                    assert curr_delta_target >= 0
+                    # ^ There should always a valid delta, since we dealt
+                    # with the max case seperately above.
+                    deltas[i,j,k] = curr_delta
+                    delta_targets[i,j,k] = curr_delta_target
                     # == end for density if-elif-else
-
-                assert done, "Problem: not done at %i, %i, %i" % (i, j, k)
 
                 gamma = deltas[i, j, k] * density[i, j, k]
                 # Go backwards since its much more likely that
@@ -177,9 +141,6 @@ cpdef dpc_compute_vars(frames_np,
                         break
 
                 if temp1 <= n_gamma - 1:
-#                     print("GAMMA: inserting %i,%i,%i at index %i with gamma %f" % (i, j, k, temp1, gamma))
-#                     print("OLD GAMMA: %s" % gamma_buf_np)
-#                     print("OLD GAMMA IDEX: %s" % gamma_idex_buf_np)
                     # Push back
                     gamma_buf[temp1 + 1:] = gamma_buf[temp1:n_gamma - 1]
                     gamma_idex_buf[temp1 + 1:] = gamma_idex_buf[temp1:n_gamma - 1]
@@ -187,31 +148,29 @@ cpdef dpc_compute_vars(frames_np,
                     gamma_buf[temp1] = gamma
                     gamma_idex_temp[0] = i; gamma_idex_temp[1] = j; gamma_idex_temp[2] = k
                     gamma_idex_buf[temp1] = to_linear_index_3D(gamma_idex_temp, n_boxes)
-#                     print("NEW GAMMA: %s" % gamma_buf_np)
-#                     print("NEW GAMMA IDEX: %s" % gamma_idex_buf_np)
-
                 # == end for k
             # == end for j
         # == end for i
 
-    assert done
     assert np.min(delta_targets_np) >= -1 # -2's are unassigned, -1 is max pt.
 
-    return density_np, point_box_assignments, deltas_np, delta_targets_np, gamma_buf_np, gamma_idex_buf_np
+    return box_vecs_np, density_np, point_box_assignments, deltas_np, delta_targets_np, gamma_buf_np, gamma_idex_buf_np
 
-cpdef dpc_assign(density_np,
-                 point_box_assignments,
-                 deltas_np,
-                 delta_targets_np,
-                 precision density_threshold,
-                 precision delta_threshold,
-                 bint include_halos = False):
+
+cpdef list dpc_assign(box_vecs_np,
+                      density_np,
+                      point_box_assignments,
+                      deltas_np,
+                      delta_targets_np,
+                      precision density_threshold,
+                      precision delta_threshold,
+                      int n_halo_boxes = 0):
     """
     Returns:
         None. Fills point_box_assignments in-place with cluster assignments.
     """
     # === Assign boxes by DPC procedure
-    cdef Py_ssize_t i,j,k, iteration, f_idex, pt_idex
+    cdef Py_ssize_t i,j,k, iteration, f_idex, pt_idex, component
     cdef Py_ssize_t idex_triple[3]
     # Nothing past this point is in space, so we proceed using linear
     # indexes and unraveled arrays:
@@ -222,6 +181,7 @@ cpdef dpc_assign(density_np,
     cdef Py_ssize_t [:] delta_targets_linear = delta_targets_np.ravel()
     cdef precision [:, :, :] density = density_np
     cdef precision [:, :, :] deltas = deltas_np
+    cdef precision [:] density_linear = density_np.ravel()
 
     # == Decide centers
     cdef list centers = []
@@ -232,17 +192,16 @@ cpdef dpc_assign(density_np,
                    deltas[i,j,k] >= delta_threshold:
                     idex_triple[0] = i; idex_triple[1] = j; idex_triple[2] = k
                     centers.append(to_linear_index_3D(idex_triple, n_boxes))
-    print(centers)
 
     # == Assign boxes to clusters
-    cdef int [:] box_cluster_assignments = np.full(n_total_boxes, CLUSTER_UNASSIGNED, dtype = np.intc)
+    box_cluster_assignments_np = np.full(n_total_boxes, CLUSTER_UNASSIGNED, dtype = np.intc)
+    cdef int [:] box_cluster_assignments = box_cluster_assignments_np
     # Assign centers to themselves
     cdef Py_ssize_t center, center_id
     for center_id, center in enumerate(centers):
         box_cluster_assignments[center] = center_id
 
-    print(box_cluster_assignments)
-
+    # Assign all boxes to clusters
     cdef bint done = False
     for iteration in range(n_total_boxes):
         done = True
@@ -252,11 +211,69 @@ cpdef dpc_assign(density_np,
                 box_cluster_assignments[i] = box_cluster_assignments[delta_targets_linear[i]]
             done = done & (not (box_cluster_assignments[i] == _CLUSTER_UNASSIGNED_C))
 
+        if done:
+            break
+
     assert done, "Somehow, box cluster assignment didn't finish. That shouldn't happen."
 
-    if not include_halos:
-        pass # TODO: the thing from the paper to find halos.
-        # Mark halo boxes with lower certainty in SiteTrajectory
+    # Make density zero boxes unassigned
+    for i in range(n_total_boxes):
+        if density_linear[i] == 0:
+            box_cluster_assignments[i] = _CLUSTER_UNASSIGNED_C
+
+    cdef precision [:] mean_halo_densities = np.zeros(shape = len(centers), dtype = density_np.dtype)
+    cdef int [:] halos_count = np.zeros(shape = len(centers), dtype = np.intc)
+    cdef precision dist_to_different
+    cdef Py_ssize_t who_is_different
+    cdef int [:, :, :] box_clusters_grid
+    cdef int how_many_halos = 0
+    if n_halo_boxes > 0:
+        # == From the Rodriguez and Laio paper ==
+        # Instead, we firstfind for each cluster a border region,
+        # defined asthe set of points assigned to that cluster but
+        # being within a distance dc from data points belonging to
+        # other clusters. We then find, for each cluster, the
+        # point of highest density within its border region.
+        # We denote its density by rho. The points of the cluster
+        # whose density is higher thanrbare consid-ered part of
+        # the cluster core (robust assignation).The others are considered
+        # part of the cluster halo (suitable to be considered as noise).
+        #
+        box_clusters_grid = box_cluster_assignments_np.reshape(density_np.shape)
+        for i in range(n_boxes[0]):
+            for j in range(n_boxes[1]):
+                for k in range(n_boxes[2]):
+                    if box_clusters_grid[i,j,k] == _CLUSTER_UNASSIGNED_C:
+                        continue # We don't care, it isn't in a cluster
+
+                    dist_to_different, who_is_different = min_dist_for_relation_pbc[int](
+                        i,j,k,
+                        box_clusters_grid, # as a grid
+                        n_boxes, box_vecs_np,
+                        RELATION_NE,
+                        n_halo_boxes + 1,
+                    )
+                    if who_is_different >= 0:
+                        how_many_halos += 1
+                        #print(dist_to_different)
+                        halos_count[box_clusters_grid[i,j,k]] += 1
+                        mean_halo_densities[box_clusters_grid[i,j,k]] += density[i,j,k]
+                        # We're in a halo
+                        # if bigger, update the max
+#                         if density[i,j,k] > max_halo_densities[box_clusters_grid[i,j,k]]:
+#                             max_halo_densities[box_clusters_grid[i,j,k]] = density[i,j,k]
+
+        # Do average
+        for i in range(len(centers)):
+            mean_halo_densities[i] /= halos_count[i]
+
+        print("Found %i halos" % how_many_halos)
+        # Mark boxes with lower densities as unassigned.
+        for i in range(n_total_boxes):
+            if density_linear[i] < mean_halo_densities[box_cluster_assignments[i]]:
+                box_cluster_assignments[i] = _CLUSTER_UNASSIGNED_C
+
+        # == end halos
 
     # == Assign points to clusters
     # (use the point_box_assignments array in place, since it's of the right
@@ -268,4 +285,98 @@ cpdef dpc_assign(density_np,
         for pt_idex in range(point_box_assignments_c.shape[1]):
             point_box_assignments_c[f_idex, pt_idex] = box_cluster_assignments[point_box_assignments_c[f_idex, pt_idex]]
 
-    return None
+    return centers
+
+
+
+cdef enum RELATION:
+    RELATION_GT = 0
+    RELATION_NE = 1
+
+#TODO: file bug on const with fused type
+
+ctypedef fused gridtype:
+    precision
+    int
+
+cdef inline (precision, Py_ssize_t) min_dist_for_relation_pbc(const Py_ssize_t i,
+                                                              const Py_ssize_t j,
+                                                              const Py_ssize_t k,
+                                                              gridtype [:, :, :] grid,
+                                                              const Py_ssize_t* n_boxes,
+                                                              const cell_precision [:, :] box_vecs,
+                                                              const RELATION relation,
+                                                              const Py_ssize_t max_radius,):
+    """
+    In a periodic grid, find the minimum distance from box (i,j,k) to another
+    box whose value satisifies the given relation with box (i,j,k)'s value.
+    """
+    # Result
+    cdef precision delta = INFINITY
+    cdef Py_ssize_t delta_target = -1
+    # Offsets
+    cdef Py_ssize_t offset[3]
+    cdef Py_ssize_t offset_idex[3]
+    cdef precision offset_vec[3]
+    # Loop variables:
+    cdef Py_ssize_t radius, component, side_sign, side_i, side_j, temp1, temp2
+    cdef precision curr_dist
+    cdef bint done = False
+    # Comparison values
+    cdef gridtype value_main, value_offset
+    value_main = grid[i, j, k]
+    cdef bint curr_relation[2]
+    # Misc
+    cdef Py_ssize_t lookup_diff_component[3]
+    lookup_diff_component[0] = 1; lookup_diff_component[1] = 2; lookup_diff_component[2] = 0
+
+    for radius in range(1, max_radius): # For each box size
+        for component in range(3): # For each box dimension
+            for side_sign in range(-1, 2, 2): # For each box side --- -1 and 1
+                for side_i in range(-radius, radius + 1): # TODO: change these bounds to not double count
+                    for side_j in range(-radius, radius + 1):
+                        # Build an offset
+                        offset[0] = side_i; offset[1] = side_i; offset[2] = side_i
+                        offset[component] = side_sign * radius
+                        offset[lookup_diff_component[component]] = side_j
+                        # % deals with periodicity
+                        offset_idex[0] = (i + offset[0]) % n_boxes[0]
+                        offset_idex[1] = (j + offset[1]) % n_boxes[1]
+                        offset_idex[2] = (k + offset[2]) % n_boxes[2]
+                        # Compute the offset vector
+                        for temp1 in range(3):
+                            offset_vec[temp1] = 0
+                            for temp2 in range(3):
+                                offset_vec[temp1] += offset[temp2] * \
+                                                     box_vecs[temp2, temp1]
+                        curr_dist = sqrt(
+                            offset_vec[0]*offset_vec[0] + \
+                            offset_vec[1]*offset_vec[1] + \
+                            offset_vec[2]*offset_vec[2]
+                        )
+
+                        value_offset = grid[offset_idex[0], offset_idex[1], offset_idex[2]]
+                        curr_relation[<int>RELATION_GT] = value_offset > value_main
+                        curr_relation[<int>RELATION_NE] = value_offset != value_main
+
+                        if curr_relation[<int>relation] and curr_dist < delta: # Must be closer to be the new delta
+                            # we found a better delta
+                            delta = curr_dist # Set the new delta
+                            delta_target = to_linear_index_3D(offset_idex, n_boxes)
+                            # We've found the "up gradient" for this box.
+                            # Break out and move to the next:
+                            done = True
+
+                        # == end for side_j
+                    # == end for side_i
+                # == end for side_sign
+            # == end for component
+        # We check the whole cube first, but if we've found a
+        # delta at this radius, is is the delta: no larger
+        # radius is going to be closer.
+        if done:
+            break
+        # == end for radius
+    # We don't check done, since there could be no correct answer.
+    # Especially if we have a max_radius.
+    return delta, delta_target
